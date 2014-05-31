c9dd7e19ad92f7b15b8e3602d2cf43d6ca5a8543
Gerhard Kotze <gerhard@bundu.it>
2014-05-31 17:55:54 +0200

    Update aria2 downloader

--- couchpotato/core/downloaders/aria2/main.py
+++ couchpotato/core/downloaders/aria2/main.py
@@ -1,4 +1,5 @@
 import xmlrpclib
+import traceback
 from couchpotato.core.downloaders.base import Downloader
 from couchpotato.core.logger import CPLog
 
@@ -15,15 +16,22 @@ def true_false(val):
 
 class Aria2(Downloader):
 
-    type = ['http']
+    protocol = ['http']
 
-    def download(self, data={}, movie={}, manual=False, filedata=None):
+    def download(self, data=None, media=None, filedata=None):
+        if not media: media = {}
+        if not data: data = {}
+
+        if not filedata:
+            log.error('Unable to get file: %s', traceback.format_exc())
+            return False
+        
         log.info('Sending "%s" to aria2', data.get('name'))
 
         s = xmlrpclib.ServerProxy('http://%s/rpc' % self.conf('host'))
         return s.aria2.addUri([data['url']], {'pause': true_false(self.conf('pause'))})
 
-    def getAllDownloadStatus(self):
+    def getAllDownloadStatus(self, ids):
         """
         status = {'id': '', 'name': '', 'status': '', 'original_status': '', 'timeleft': ''}
         """
